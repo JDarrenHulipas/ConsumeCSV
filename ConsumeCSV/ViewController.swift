b9130f58647ef4fd3c179bb561b32469c705a065
John Darren Hulipas <darrenhulipas@gmail.com>
2019-03-06 19:25:33 +0100

    Initial Commit

---
+++ ConsumeCSV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConsumeCSV
+//
+//  Created by John Darren on 06/03/2019.
+//  Copyright © 2019 Nerrad Sapiluh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
